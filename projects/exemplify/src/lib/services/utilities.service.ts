1c0a569dc492492075368ec6d272350bd64a2f63
Robert Hjalmers <robert.hjalmers@seb.se>
2018-08-16 09:12:50 +0200

    fix: don't throw error when using `selector` in aot build
    Exemplify throws an error when using the selector input in aot builds, it should warn but not break
    the app.
    
    #15

--- projects/exemplify/src/lib/services/utilities.service.ts
+++ projects/exemplify/src/lib/services/utilities.service.ts
@@ -10,10 +10,15 @@ export class UtilitiesService {
   /* parse html from and return element matched with selector */
   parseHtml(template: ViewContainerRef | string, selector: string) {
     const parser = new DOMParser();
-    const htmlTemplate = typeof template !== 'string' ? template[ '_data' ].componentView.component.viewContainerRef[ '_view' ].component.constructor['__annotations__'][0].template : template;
+    let htmlTemplate = '';
+    try {
+      htmlTemplate = typeof template !== 'string' ? template[ '_data' ].componentView.component.viewContainerRef[ '_view' ].component.constructor['__annotations__'][0].template : template;
+    } catch (e) {
+        console.warn('Exemplify: couldn\'t retrieve html for "' + selector + '", most likely due to annotations being removed with aot build. Referer to the following issue for more info and workaround:', 'https://github.com/hjalmers/angular-exemplify/issues/15');
+    }
     let output = '';
     const markupExampleCode = parser.parseFromString(htmlTemplate, 'text/html').querySelectorAll(selector);
-    if (markupExampleCode.length === 0) {
+    if (markupExampleCode.length === 0 && htmlTemplate !== '') {
       console.warn('Exemplify: no element matched selector, no markup returned.');
     }
     for (let i = 0; i < markupExampleCode.length; i++) {
