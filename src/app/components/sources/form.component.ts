5460f736fb7170bbcf2d011053819d723bf1a7b2
Robert Hjalmers <robert.hjalmers@gmail.com>
2019-04-06 18:24:11 +0200

    docs: update example

--- src/app/components/sources/form.component.ts
+++ src/app/components/sources/form.component.ts
@@ -9,7 +9,8 @@ export class FormComponent {
   externalSources = [{
     name: 'form.component.html',
     src: 'https://raw.githubusercontent.com/hjalmers/angular-markup-example/master/src/app/components/sources/form.component.html',
-    lang: 'markup'
+    lang: 'markup',
+    selector: 'form'
   }, {
     name: 'form.component.ts',
     src: 'https://raw.githubusercontent.com/hjalmers/angular-markup-example/master/src/app/components/sources/form.component.ts',
@@ -24,6 +25,35 @@ export class FormComponent {
     lang: 'markup',
     selector: '.alert.alert-info'
   }];
+
+  /* load meta example */
+  metaExampleConfig = [
+    {
+      name: 'source config',
+      src: `
+  externalSources = [{
+    name: 'form.component.html',
+    src: 'https://raw.githubusercontent.com/hjalmers/angular-markup-example/master/src/app/components/sources/form.component.html',
+    lang: 'markup',
+    selector: 'form'
+  }, {
+    name: 'form.component.ts',
+    src: 'https://raw.githubusercontent.com/hjalmers/angular-markup-example/master/src/app/components/sources/form.component.ts',
+    lang: 'typescript'
+  }, {
+    name: 'form.component.scss',
+    src: 'https://raw.githubusercontent.com/hjalmers/angular-markup-example/master/src/app/components/sources/form.component.scss',
+    lang: 'css'
+  }, {
+    name: 'markup for alert',
+    src: 'https://raw.githubusercontent.com/hjalmers/angular-markup-example/master/src/app/components/sources/form.component.html',
+    lang: 'markup',
+    selector: '.alert.alert-info'
+  }]
+    `,
+      lang: 'javascript'
+    }
+  ];
 
   constructor() {}
 
